1bf7630abe5e210b45bb58aa5e89bf41d0bc9c2e
youssef-38 <youssefuni23@gmail.com>
2026-03-21 03:12:52 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'core/theme.dart';
-import 'core/app_router.dart'
+import 'core/theme.dart'
+import 'core/app_router.dart';
 
 void main() {
   runApp(const AeroLineApp());
